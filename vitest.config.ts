9a393a353461c264c7c9685d76885f8d77a576ae
Alexander Ekaristiawan <134070621+AlexanderEkaristiawan@users.noreply.github.com>
2026-05-22 19:21:40 +0800

    test fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,15 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       setupFiles: ['./src/test-setup.ts'],
+      env: {
+        VITE_FIREBASE_API_KEY: 'mock-api-key',
+        VITE_FIREBASE_AUTH_DOMAIN: 'mock-auth-domain',
+        VITE_FIREBASE_PROJECT_ID: 'mock-project-id',
+        VITE_FIREBASE_STORAGE_BUCKET: 'mock-storage-bucket',
+        VITE_FIREBASE_MESSAGING_SENDER_ID: 'mock-sender-id',
+        VITE_FIREBASE_APP_ID: 'mock-app-id',
+        VITE_FIREBASE_MEASUREMENT_ID: 'mock-measurement-id',
+      },
     },
   }),
 )
